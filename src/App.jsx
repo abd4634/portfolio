7771d647dce794e85cef02ef9b87dd1462d2f2ae
Abdullah <abd4634abd@gmail.com>
2025-01-01 08:50:45 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import About from './component/About';
-import Cards from './component/cards';
 import Contact from './component/contact';
 import Hero from './component/hero';
 import Navbar from './component/navbar';
