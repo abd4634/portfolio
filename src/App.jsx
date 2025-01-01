b5b19264ff48f206f0c0efefc8699291cdd2239e
Abdullah <abd4634abd@gmail.com>
2025-01-01 08:37:42 +0530

    update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import About from './component/About';
+import Cards from './component/cards';
+import Contact from './component/contact';
+import Hero from './component/hero';
+import Navbar from './component/navbar';
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <About/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App
